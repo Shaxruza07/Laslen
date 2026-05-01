eb1c08524edfd0e1a5d8ea23dad0ed0000f1d0b3
Shaxruza07 <rejabovashakhruza@gmail.com>
2026-05-01 17:51:17 +0500

    first commit

--- main.js
+++ main.js
@@ -9,3 +9,35 @@ bars.addEventListener("click", function () {
 closeBtn.addEventListener("click", function () {
     menu.classList.remove("show-menu");
 });
+
+// caousel
+let carouselInner = document.querySelector(".carousel_inner")
+let dots = document.querySelectorAll(".dot")
+let current = 0;
+
+function changeSlide() {
+    carouselInner.style.transform = `translateX(-${current * 100}%)`;
+
+    dots.forEach(function (dot) {
+        dot.classList.remove("active");
+    });
+
+    dots[current].classList.add("active");
+}
+
+dots.forEach(function (dot, index) {
+    dot.addEventListener("click", function() {
+        current = index;
+        changeSlide()
+    });
+});
+
+setInterval(function () {
+    current++;
+
+    if (current === dots.length) {
+        current = 0
+    }
+
+    changeSlide();
+}, 3000);
