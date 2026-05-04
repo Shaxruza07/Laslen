4c0f07f354c1e2f6da731dc697a16fe1f780dadb
Shaxruza07 <rejabovashakhruza@gmail.com>
2026-05-04 18:08:37 +0500

    first-commit

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 let bars = document.querySelector(".bars");
 let menu = document.querySelector(".menu");
 let closeBtn = document.querySelector(".close-btn");
+const modal = document.getElementById("modal");
+const btn = document.getElementById("openModal");
+const close = document.querySelector(".close");
+
+btn.onclick = () => {
+  modal.style.display = "block";
+};
+
+close.onclick = () => {
+  modal.style.display = "none";
+};
+
+window.onclick = (e) => {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+};
 
 bars.addEventListener("click", function () {
     menu.classList.add("show-menu");
